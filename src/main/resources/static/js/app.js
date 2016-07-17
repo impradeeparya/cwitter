dfb52c44fd1cf3a663365f965f49bb708a700ad4
Pradeep Arya <parya@xebia.com>
2016-07-17 16:53:02 +0530

    front end base structure and login form added

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pradeep
+ * Date: 17/7/16
+ * Time: 3:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/main/resources/static/js/cwitter.js
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pradeep
+ * Date: 17/7/16
+ * Time: 4:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$('.message a').click(function () {
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
